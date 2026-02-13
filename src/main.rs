3ae1a87e8912220cc1c0bd55217a536940f2ef23
Linnea Gräf <nea@nea.moe>
2026-02-14 00:41:52 +0100

    feat: add public room indexing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use axum::{Json, Router, extract::State, response::IntoResponse, routing::get};
+use axum::{
+	Json, Router,
+	extract::{Query, State},
+	http::StatusCode,
+	response::IntoResponse,
+	routing::get,
+};
 use serde::{Deserialize, Serialize};
-use std::sync::Arc;
+use std::{sync::Arc, usize};
+use tracing::error;
 
 #[derive(Serialize)]
 struct WellKnownServerResponse {
@@ -24,6 +31,7 @@ struct SupportContact {
 struct Config {
 	contact: Option<SupportContact>,
 	delegate_url: String,
+	public_rooms: Vec<PublicRoom>,
 }
 
 #[derive(Clone)]
@@ -31,8 +39,48 @@ struct AppState {
 	config: Arc<Config>,
 }
 
+type Result<T, E = Report> = eyre::Result<T, E>;
+struct Report(eyre::Report);
+impl std::fmt::Debug for Report {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		self.0.fmt(f)
+	}
+}
+impl<E> From<E> for Report
+where
+	E: Into<eyre::Report>,
+{
+	fn from(value: E) -> Self {
+		Self(value.into())
+	}
+}
+impl IntoResponse for Report {
+	fn into_response(self) -> axum::response::Response {
+		let Self(err) = self;
+		let error_msg = format!("{err}");
+
+		error!(?err);
+
+		(
+			StatusCode::INTERNAL_SERVER_ERROR,
+			Json(serde_json::json!({
+				"errcode": "M_UNKNOWN",
+				"error": error_msg,
+			})),
+		)
+			.into_response()
+	}
+}
+macro_rules! ensure {
+	($cond:expr, $($rest:tt)*) => {
+		if !($cond) {
+			let _discard: () = Err(eyre::eyre!($($rest)*))?;
+		}
+	};
+}
+
 #[tokio::main]
-async fn main() -> eyre::Result<()> {
+async fn main() -> Result<()> {
 	tracing_subscriber::fmt::init();
 	let config: Config =
 		serde_json::de::from_str(&tokio::fs::read_to_string("config.json").await?)?;
@@ -49,6 +97,7 @@ async fn main() -> eyre::Result<()> {
 		.route("/.well-known/matrix/server", get(well_known_server))
 		.route("/.well-known/matrix/support", get(well_known_support))
 		.route("/_matrix/federation/v1/version", get(version))
+		.route("/_matrix/federation/v1/publicRooms", get(public_rooms))
 		.with_state(AppState {
 			config: config.clone(),
 		});
@@ -56,6 +105,81 @@ async fn main() -> eyre::Result<()> {
 
 	axum::serve(listener, router).await?;
 	Ok(())
+}
+
+#[derive(Deserialize)]
+struct PublicRoomQuery {
+	#[serde(default)]
+	include_all_networks: bool,
+	#[serde(default)]
+	limit: usize,
+	#[serde(default)]
+	since: Option<String>,
+}
+
+#[derive(Serialize)]
+struct PublicRoomResponse {
+	chunk: Vec<PublicRoom>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	prev_batch: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	next_batch: Option<String>,
+	total_room_count_estimate: usize,
+}
+#[derive(Clone, Deserialize, Serialize, Debug)]
+struct PublicRoom {
+	#[serde(skip_serializing_if = "Option::is_none", default)]
+	avatar_url: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none", default)]
+	canonical_alias: Option<String>,
+	guest_can_join: bool,
+	#[serde(skip_serializing_if = "Option::is_none", default)]
+	join_rule: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none", default)]
+	name: Option<String>,
+	num_joined_members: u32,
+	room_id: String,
+	room_type: String,
+	#[serde(skip_serializing_if = "Option::is_none", default)]
+	topic: Option<String>,
+	world_readable: bool,
+}
+
+#[axum::debug_handler]
+async fn public_rooms(
+	Query(query): Query<PublicRoomQuery>,
+	State(state): State<AppState>,
+) -> Result<impl IntoResponse> {
+	let offset: usize = match query.since {
+		Some(s) => s.parse()?,
+		None => 0,
+	};
+	let rooms = &state.config.public_rooms;
+	let limit = if query.limit == 0 {
+		usize::MAX
+	} else {
+		offset.saturating_add(query.limit)
+	};
+	ensure!(offset <= rooms.len(), "Since too big");
+	let view = if query.include_all_networks {
+		&rooms[offset..(limit.min(rooms.len()))]
+	} else {
+		&[]
+	};
+	Ok(Json(PublicRoomResponse {
+		total_room_count_estimate: rooms.len(),
+		chunk: view.into(),
+		prev_batch: if limit == usize::MAX || view.is_empty() || offset == 0 {
+			None
+		} else {
+			Some(offset.saturating_sub(limit).to_string())
+		},
+		next_batch: if limit == usize::MAX || view.is_empty() || limit >= rooms.len() {
+			None
+		} else {
+			Some(offset.saturating_add(limit).to_string())
+		},
+	}))
 }
 
 async fn version() -> impl IntoResponse {
